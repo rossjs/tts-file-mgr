adf6e63bb7e1dbf2a378429706eb35250d773b2b
Ross Topol <rtopol2@gmail.com>
2021-04-04 18:28:10 -0500

    add note about default OS locations

--- server/config/index.js
+++ server/config/index.js
@@ -2,6 +2,7 @@ const path = require('path');
 const os = require('os');
 
 const config = {
+  // TODO: set based on OS in default location
   modsDirectory: path.resolve(os.homedir(), 'Desktop/test-files/tts_mods'),
 };
 module.exports = config;
